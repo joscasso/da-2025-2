ee919618cf0e95abbe165a2fbdc0d88236c6a34a
joscasso <18463956+joscasso@users.noreply.github.com>
2025-11-13 19:46:10 -0500

    Date 13/11/2025 3

--- taller-auth/vite.config.ts
+++ taller-auth/vite.config.ts
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'; // 👈 Importa tailwindcss
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),], 
+
+ build: {
+    target: 'es2020', // Mantener el target de producción
+  },  
 })
